98e8732760d2d4a35f7ced5bfe3fc7e45412cdbe
epait <ericpait@gmail.com>
2014-02-10 13:03:21 -0500

    adding models

--- roster/models.py
+++ roster/models.py
@@ -2,23 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Student(models.Model):
-	name = models.CharField(unique=True, max_length=50)
-	pid = models.CharField(unique=True, max_length=12)
-	grade = models.IntegerField(max_length=3)
-
-	class Meta(object):
-		ordering = ('pid', 'name')
-
-	def __unicode__(self):
-		return U'%s %s' %(self.name, self.pid)
-
 class Course(models.Model):
 	name = models.CharField(unique=True, max_length=50)
 	instructor = models.CharField(max_length=50)
 	description = models.CharField(max_length=200)
 	term = models.CharField(max_length=50)
-	students = models.ManyToManyField(Student)
+	students = models.ManyToManyField('Student')
 	date = models.DateField()
 
 	class Meta(object):
@@ -32,3 +21,13 @@ class Course(models.Model):
 		self.name = self.name.upper()
 		super(Course, self).save(*args, **kwargs)
 
+class Student(models.Model):
+	name = models.CharField(unique=True, max_length=50)
+	pid = models.CharField(unique=True, max_length=12)
+	grade = models.IntegerField(max_length=3)
+
+	class Meta(object):
+		ordering = ('pid', 'name')
+
+	def __unicode__(self):
+		return U'%s %s' %(self.name, self.pid)
